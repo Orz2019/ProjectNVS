beb9fa19dd3568bfaa9b5372a97edd96509b25df
ZapperSan <57942278+ZapperSan@users.noreply.github.com>
2019-11-19 12:38:53 +0100

    Hotfix 1.1.1
    Added restriction to maximum number user can define.

--- Project/main.c
+++ Project/main.c
@@ -5,10 +5,16 @@ int main()
 {
     int cislo = 0;
     int soustava;
-    printf("Prosim zadejte cislo v desitkove soustave, ktere chcete prevest.\n");
+    START:printf("Prosim zadejte cislo v desitkove soustave, ktere chcete prevest (Max.1000).\n");
     scanf("%d",&cislo);
+        if(cislo >1000)
+    {
+        printf("Vami zadane cislo je moc vysoke.\n");
+        goto START;
+    }
     printf("Prosim zadejte typ soustavy:\n [2] = Binarni soustava\n [8] = Oktalni soustava\n [16] = Hexadekalni soustava\n");
     scanf("%d",&soustava);
+
     switch(soustava)
         {
         case 2:
